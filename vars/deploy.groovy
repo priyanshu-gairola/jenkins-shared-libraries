ba3d59ad2b50fbb74812d96235cda14548426b81
Priyanshu Gairola <gairola.priyanshu03@gmail.com>
2026-09-04 15:17:34 +0530

    Update deploy.groovy

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -1,3 +1,4 @@
-def call() {
-  echo "hello team , lets deploy"
+def call(String project, String tag) 
+{
+    sh "docker rm -f ${project} || true && docker run -d --name ${project} -p 8080:8080 ${DOCKERHUB_USER}/${project}:${tag}"
 }
